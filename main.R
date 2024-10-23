41d4387c428cbdd08c5b2fa0b87164cc11f73be2
DamienCKj2812 <damienckj5993@gmail.com>
2024-10-23 22:51:07 +0800

    created folder for each member

--- main.R
+++ main.R
@@ -1,17 +1,1 @@
-library(tidyverse)
 
-data <- read.csv("5. credit_risk_classification.csv")
-
-# Assuming 'data' is a data frame and contains the columns you're interested in
-# Selecting specific columns
-selected_columns <- data %>%
-  select(name, height, ends_with("free"))
-
-# View the selected columns
-print(selected_columns)
-
-<<<<<<< HEAD
-=======
-print("Hello world")
-print("hi")
->>>>>>> c3e38e95e3f52df74389a72b37b5fbbe2ea20366
