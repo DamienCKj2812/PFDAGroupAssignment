2fe22faa152a8aaa056ec097cce875faad57e735
DamienCKj2812 <damienckj5993@gmail.com>
2024-10-23 22:42:52 +0800

    testing

--- main.R
+++ main.R
@@ -1,2 +1,12 @@
+library(tidyverse)
 
-print("Hello world")
+data <- read.csv("5. credit_risk_classification.csv")
+
+# Assuming 'data' is a data frame and contains the columns you're interested in
+# Selecting specific columns
+selected_columns <- data %>%
+  select(name, height, ends_with("free"))
+
+# View the selected columns
+print(selected_columns)
+
